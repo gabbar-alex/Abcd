41a39d0b9a00e233daad945eab8e95c0bbaefa61
gabbar-alex <bantikumar3675@gmail.com>
2025-05-01 21:26:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
+api_id = "26091366"
+api_hash = "43da5693f3a6dbb36f57b37c4365936c"
 bot_token = "enter your bot token"
 auth_users = [7991495621]
